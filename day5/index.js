1955ef2def1c8adfa536243d283761c4c4b216c1
jsakyle@gmail.com <jsakyle@gmail.com>
2021-12-06 02:46:50 -0500

    cleaned up day 5

--- day5/index.js
+++ day5/index.js
@@ -16,7 +16,10 @@ function Part1() {
 }
 
 function Part2() {
-  console.log('part2');
+  const input = helpers.getInputAsStringArray('day5/input.txt');
+  const formattedInput = FormatInput(input);
+
+  CreateMap(formattedInput, false);
 }
 
 /**
@@ -32,115 +35,47 @@ function FormatInput(input) {
 }
 
 
-// rewrite this to be easier to understand
 /**
  * 
  * @param {string[][]} input 
+ * @param {boolean} skipDiagonals
  */
-function CreateMap(input) {
+function CreateMap(input, skipDiagonals = true) {
   const ventMap = new Map();
-  input.map((coordinateSet) => {
+
+  input.forEach((coordinateSet) => {
     const coords = coordinateSet.map((coord) => coord.split(',').map((num) => parseInt(num)));
-    // if xs match
-    if((coords[0][0] === coords[1][0])) {
-      if(coords[0][1] > coords[1][1]) {
-        for(let i = coords[0][1]; i >= coords[1][1]; i--) {
-          if(ventMap.has(`${coords[0][0]},${i}`)) {
-            ventMap.set(`${coords[0][0]},${i}`, ventMap.get(`${coords[0][0]},${i}`) + 1);
-          } else {
-            ventMap.set(`${coords[0][0]},${i}`, 1);
-          }
-        }
-      } else {
-        for(let i = coords[0][1]; i <= coords[1][1]; i++) {
-          if(ventMap.has(`${coords[0][0]},${i}`)) {
-            ventMap.set(`${coords[0][0]},${i}`, ventMap.get(`${coords[0][0]},${i}`) + 1);
-          } else {
-            ventMap.set(`${coords[0][0]},${i}`, 1);
-          }
+      // skip diag for part 1
+      if(skipDiagonals) {
+        if((coords[0][0] !== coords[1][0]) && (coords[0][1] !== coords[1][1])) {
+          return
         }
       }
-      // y's match
-    } else if((coords[0][1] === coords[1][1])) {
-      if(coords[0][0] > coords[1][0]) {
-        for(let i = coords[0][0]; i >= coords[1][0]; i--) {
-          if(ventMap.has(`${i},${coords[0][1]}`)) {
-            ventMap.set(`${i},${coords[0][1]}`, ventMap.get(`${i},${coords[0][1]}`) + 1);
-          } else {
-            ventMap.set(`${i},${coords[0][1]}`, 1);
-          }
-        }
-      } else {
-        for(let i = coords[0][0]; i <= coords[1][0]; i++) {
-          if(ventMap.has(`${i},${coords[0][1]}`)) {
-            ventMap.set(`${i},${coords[0][1]}`, ventMap.get(`${i},${coords[0][1]}`) + 1);
-          } else {
-            ventMap.set(`${i},${coords[0][1]}`, 1);
-          }
-        }
-      }
-    }
-    //diagonal 
-    else if((coords[0][0] === coords[0][1]) && (coords[1][0] === coords[1][1])) {
-      console.log(`diag ${coords[0]} ${coords[1]}`)
-      if(coords[0][0] > coords[1][0]) {
-        for(let i = coords[0][0]; i >= coords[1][0]; i--) {
-          if(ventMap.has(`${i},${i}`)) {
-            ventMap.set(`${i},${i}`, ventMap.get(`${i},${i}`) + 1);
-          } else {
-            ventMap.set(`${i},${i}`, 1);
-          }
-        }
-      } else {
-        for(let i = coords[0][0]; i <= coords[1][0]; i++) {
-          if(ventMap.has(`${i},${i}`)) {
-            ventMap.set(`${i},${i}`, ventMap.get(`${i},${i}`) + 1);
-          } else {
-            ventMap.set(`${i},${i}`, 1);
-          }
-        }
-      }
-    } else if((coords[0][0] === coords[1][1]) && (coords[0][1] === coords[1][0])) {
-      console.log(`diag ${coords[0]} ${coords[1]}`)
-      if(coords[0][0] > coords[1][0]) {
-        for(let i = 0; i <= coords[0][0] - coords[1][0]; i++) {
-          let newX = coords[0][0] - i;
-          let newY = coords[0][1] + i;
 
-          if(ventMap.has(`${newX},${newY}`)) {
-            ventMap.set(`${newX},${newY}`, ventMap.get(`${newX},${newY}`) + 1);
-          } else {
-            ventMap.set(`${newX},${newY}`, 1);
-          }
-        }
+      if(coords[0][0] === coords[1][0]) {
+        diff = Math.abs(coords[0][1] - coords[1][1])
       } else {
-        for(let i = coords[1][0] - coords[0][0]; i >= 0; i--) {
-          let newX = coords[0][0] + i;
-          let newY = coords[0][1] - i;
-
-          if(ventMap.has(`${newX},${newY}`)) {
-            ventMap.set(`${newX},${newY}`, ventMap.get(`${newX},${newY}`) + 1);
-          } else {
-            ventMap.set(`${newX},${newY}`, 1);
-          }
-        }
-      }
-    } else {
         diff = Math.abs(coords[0][0] - coords[1][0])
-        console.log(diff);
+      }
+
       for(let i = 0; i <= diff; i++) {
         let newX;
-        let newY
+        let newY;
+
         if(coords[0][0] > coords[1][0]) {
           newX = coords[0][0] - i;
-        } else {
+        } else if (coords[0][0] < coords[1][0]) {
           newX = coords[0][0] + i;
+        } else {
+          newX = coords[0][0]
         }
 
         if(coords[0][1] > coords[1][1]) {
           newY = coords[0][1] - i;
-        } else {
+        } else if (coords[0][1] < coords[1][1]) {
           newY = coords[0][1] +i;
+        } else {
+          newY = coords[0][1] 
         }
 
         if(ventMap.has(`${newX},${newY}`)) {
@@ -149,10 +84,10 @@ function CreateMap(input) {
           ventMap.set(`${newX},${newY}`, 1);
         }
       }
-    }
   })
-  //console.log(ventMap)
-  console.log(Array.from(ventMap.values()).filter((val) => val > 1).length) //part 2 - 13659 is too low
+  
+
+  console.log(Array.from(ventMap.values()).filter((val) => val > 1).length)
 }
 
 const part = process.argv[2];
